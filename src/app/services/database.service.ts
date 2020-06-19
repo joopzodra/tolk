b0a511bfbcdbc4f99ce81d8cf35a529868d83621
Joop Rosier <joop@zodra.nl>
2020-06-19 11:23:15 +0200

    ignore diacritics in database search

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -10,44 +10,50 @@ import { unhandledRejectionReasons } from '../helpers/database-unhandled-rejecti
 
 interface CustomizedValueRange {
   sheetName: string,
-  values: string[][]
+    values: string[][]
 }
 
 export interface Selection {
   searchTerm: string,
-  items: { lang1: string, lang2: string }[],
-  byBegin?: boolean,
-  count?: number
+    items: { lang1: string, lang2: string }[],
+    byBegin ? : boolean,
+    count ? : number
 }
 
 interface SheetMeta {
   spreadsheetTitle: string,
-  columnNames: string[],
-  sheetNames: string[]
+    columnNames: string[],
+    sheetNames: string[]
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
-  db: Database;
-  private sheetMeta = new Subject<SheetMeta>();
-  public sheetMetaStream = this.sheetMeta.asObservable();
-  private selection = new Subject<Selection>();
-  public selectionStream = this.selection.asObservable();
+  private db: Database;
+  private sheetMeta = new Subject < SheetMeta > ();
+  private selection = new Subject < Selection > ();
   private searching = new BehaviorSubject(false);
+  public sheetMetaStream = this.sheetMeta.asObservable();
+  public selectionStream = this.selection.asObservable();
   public searchingStream = this.searching.asObservable();
 
   constructor(private dialogService: DialogService) {
     this.db = new Database('TolkDatabase');
-    // Error handling in case browser data is manually deleted by user
+    // Error handling of db actions in the select method, in case browser data has manually deleted by user
     window.onunhandledrejection = event => {
       event.preventDefault();
       const rejectionReasonName = event.reason.name;
-      if (unhandledRejectionReasons[rejectionReasonName]) {
-        this.dialogService.emitMessage('warning', unhandledRejectionReasons[rejectionReasonName], 10000);
-      } else {
-        this.dialogService.emitMessage('danger', event.reason.message, 10000);
+      switch (event.reason.name) {
+        case 'DatabaseClosedError':
+          this.dialogService.emitMessage('warning', nl.DATABASE_CLOSED_ERROR, 10000);
+          break;
+        case 'InvalidStateError':
+          this.dialogService.emitMessage('warning', nl.DATABASE_INVALID_STATE_ERROR, 10000);
+          break;
+        default:
+          this.dialogService.emitMessage('danger', event.reason.message, 10000);
+          break;
       }
     };
   }
@@ -56,9 +62,9 @@ export class DatabaseService {
     const prepVals = this.prepareValues(customizedValueRanges);
 
     return this.db.transaction('rw', this.db.glossery, () => {
-      this.db.glossery.clear();
-      this.db.glossery.bulkAdd(prepVals);
-    })
+        this.db.glossery.clear();
+        this.db.glossery.bulkAdd(prepVals);
+      })
       .then(result => {
         localStorage.setItem('spreadsheetTitle', spreadsheetTitle);
         localStorage.setItem('columnNames', JSON.stringify(columnNames));
@@ -85,23 +91,24 @@ export class DatabaseService {
     }
 
     const dbTable = this.db.glossery;
-    const maybeByBegin = (dbTable: Dexie.Table<SheetRow, number>) => {
+    const maybeByBegin = (dbTable: Dexie.Table < SheetRow, number > ) => {
       if (byBegin) {
-        return dbTable.where(column).startsWithIgnoreCase(term);
+        return dbTable.filter(words => words[column].normalize('NFD').replace(/[\u0300-\u036f]/g, '').startsWith(term))
       } else {
-        const escapeRegExpTerm = term.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-        const regex = new RegExp(escapeRegExpTerm, 'i');
-        return dbTable.filter(words => regex.test(words[column]));
+        // escape the special regex characters in the term, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+        const escapedTerm = term.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+        const regex = new RegExp(escapedTerm, 'i');
+        // normalize('NDF') etc, see https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+        return dbTable.filter(words => regex.test(words[column].normalize('NFD').replace(/[\u0300-\u036f]/g, '')));
       }
     }
-    const maybeSheet = (whereClause: Dexie.Collection<SheetRow, number>) => {
+    const maybeSheet = (whereClause: Dexie.Collection < SheetRow, number > ) => {
       return sheet ? whereClause.and(row => row.sheetName === sheet) : whereClause;
     }
 
     const selectionCollection = maybeSheet(maybeByBegin(dbTable));
     const count = selectionCollection.count();
-    const results: Promise<Selection> = selectionCollection
-      .limit(7)
+    const results: Promise < Selection > = selectionCollection
       .toArray()
       .then(result => {
         const picked = result.map(row => ({ lang1: row.lang1, lang2: row.lang2 }));
